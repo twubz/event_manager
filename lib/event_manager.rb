cdaf6e30a42ae96bc19913b193c3148b0ba08abc
Angelo Williams <angelowilliams17@gmail.com>
2018-02-08 22:18:07 -0500

    Add basic event_manager

---
+++ lib/event_manager.rb
@@ -0,0 +1,15 @@
+require "csv"
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+puts "EventManager initialized."
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  zipcode = clean_zipcode(zipcode)
+  puts "#{name}: #{zipcode}"
+end
